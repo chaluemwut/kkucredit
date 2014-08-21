fa7563051cad10e563938cb5e18e26d4a4cce12e
Computer Engineering <ECP@computer-engineerings-imac91.local>
2014-08-21 15:14:03 +0700

    test

--- kku.js
+++ kku.js
@@ -10,14 +10,29 @@ feed_stream
   substream_1
 */
 
-function kkuCredit() {
-	// $("[class*='clearfix']").each(function(){
-	// 	console.log(this.innerHTML);
-	// });
-	console.log($('#contentArea > #stream_pagelet > [id*="topnews_main_stream"] > [id*="feed_stream"] > [id*="substream"]').length);
-	$('#contentArea > #stream_pagelet > [id*="topnews_main_stream"] > [id*="feed_stream"] > [id*="substream"] > div').each(function(){
-		console.log(this.innerHTML);
+$(document).ready(function () {
+	$("[id*='feed_stream_']").children().each(function(i){
+		if (i == 0) return;
+		var sub_stream = $(this);
+		var news_feed_list = sub_stream.children().children();
+		// console.log(news_feed_list.size());
+		for(var j=0; j < news_feed_list.size(); j++){
+			var news_feed = news_feed_list.get(j);
+			var news_feed_html = jQuery(news_feed);
+			var userContentWrapper = news_feed_html.children();
+			var attr = userContentWrapper[0];
+			console.log(attr);
+			// var userContentWrapper = news_feed.children();
+			// console.log(userContentWrapper);
+		}
+		// console.log(news_feed.size());
+		// console.log(fee_obj.attr('class'));
+		// var inner_obj = fee_obj.children();
+		// console.log(inner_obj.attr('class'));
+		// for(var j=0; j<inner_obj.size(); j++){
+		// 	var each_news_feed = inner_obj.get(j);
+		// 	console.log(each_news_feed.attr('class'));
+		// }
 	});
-}
-
-kkuCredit();
+ 
+});
